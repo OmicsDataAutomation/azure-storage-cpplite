6af2918d9745a7b0be0668ba4b6e81b88c35429a
Nalini Ganapati <nganapati@gmail.com>
2023-01-27 07:44:02 -0800

    Add back basic blob test

--- test/test_base.h
+++ test/test_base.h
@@ -18,7 +18,7 @@ namespace as_test {
         static const std::string& standard_storage_connection_string() {
             // see https://learn.microsoft.com/en-us/azure/storage/common/storage-configure-connection-string
             // static std::string sscs = "DefaultEndpointsProtocol=https;AccountName=storagesample;AccountKey=<account-key>"
-            static std::string sscs = "UseDevelopmentStorage=true"; // Shortcut connection string for azurite
+            static std::string sscs = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1"; // connection string for azurite
             return sscs;
         }
 
