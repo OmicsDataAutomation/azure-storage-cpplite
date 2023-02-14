0eff866d73ed57a641bd8b121417e02545b9c32c
Santhosh K M <santhosh.km@omicsautomation.com>
2023-02-14 13:11:08 +0530

    Fixed comments

--- src/openssl_shim.cpp
+++ src/openssl_shim.cpp
@@ -9,9 +9,11 @@
 namespace azure {  namespace storage_lite {
 
 #ifdef __APPLE__
-  std::vector<std::string> dl_paths_ = {"/usr/local/Cellar/lib/", "/usr/local/lib/", "/usr/lib/", ""};
+  std::vector<std::string> dl_paths_ = {"/usr/local/Cellar/lib/", 
+      "/usr/local/lib/", "/usr/lib/", ""};
 #elif __linux__
-  std::vector<std::string> dl_paths_ = {"/usr/lib64/", "/usr/lib/", "/usr/lib/x86_64-linux-gnu", ""};
+  std::vector<std::string> dl_paths_ = {"/usr/lib64/", "/usr/lib/", 
+      "/usr/lib/x86_64-linux-gnu", ""};
 #else
 #  error Platform not supported
 #endif
@@ -28,11 +30,15 @@ namespace azure {  namespace storage_lite {
   void (*hmac_ctx_free_fptr)(void *);
   void* (*evp_mac_fetch_fptr)(void *, const char *, const char *);
   void* (*evp_mac_ctx_new_fptr)(void *);
-  OSSL_PARAM_OSSL3_SHIM (*ossl_param_construct_utf8_string_fptr)(const char *, char *, size_t);
+  OSSL_PARAM_OSSL3_SHIM (*ossl_param_construct_utf8_string_fptr)
+      (const char *, char *, size_t);
   OSSL_PARAM_OSSL3_SHIM (*ossl_param_construct_end_fptr)(void);
-  int (*evp_mac_init_fptr)(void *, const unsigned char *, size_t, const OSSL_PARAM_OSSL3_SHIM []);
-  int (*evp_mac_update_fptr)(void *ctx, const unsigned char *data, size_t datalen);
-  int (*evp_mac_final_fptr)(void *ctx, unsigned char *out, size_t *outl, size_t outsize);
+  int (*evp_mac_init_fptr)(void *, const unsigned char *, size_t, 
+      const OSSL_PARAM_OSSL3_SHIM []);
+  int (*evp_mac_update_fptr)(void *ctx, const unsigned char *data, 
+      size_t datalen);
+  int (*evp_mac_final_fptr)(void *ctx, unsigned char *out, size_t *outl, 
+      size_t outsize);
 
 
   void *get_dlopen_handle(const std::string& name, const std::string& version) {
@@ -82,21 +88,35 @@ namespace azure {  namespace storage_lite {
       }
       ossl_ver = OpenSSL_version_num();
       if(ossl_ver < 0x30000000L) {
-        BIND_SYMBOL(dl_handle, hmac_ctx_new_fptr, "HMAC_CTX_new", (void*(*)(void)) );
-        BIND_SYMBOL(dl_handle, hmac_ctx_reset_fptr, "HMAC_CTX_reset", (int(*)(void*)) );
-        BIND_SYMBOL(dl_handle, hmac_init_ex_fptr, "HMAC_Init_ex", (int(*)(void *, const void *, int , const void *, void *)) );
-        BIND_SYMBOL(dl_handle, hmac_update_fptr, "HMAC_Update", (int(*)(void *, const unsigned char *, size_t)) );
-        BIND_SYMBOL(dl_handle, hmac_final_fptr, "HMAC_Final", (int(*)(void *, unsigned char *, unsigned int *)) );
-        BIND_SYMBOL(dl_handle, hmac_ctx_free_fptr, "HMAC_CTX_free", (void(*)(void *)) );
+        BIND_SYMBOL(dl_handle, hmac_ctx_new_fptr, "HMAC_CTX_new", 
+            (void*(*)(void)) );
+        BIND_SYMBOL(dl_handle, hmac_ctx_reset_fptr, "HMAC_CTX_reset", 
+            (int(*)(void*)) );
+        BIND_SYMBOL(dl_handle, hmac_init_ex_fptr, "HMAC_Init_ex", 
+            (int(*)(void *, const void *, int , const void *, void *)) );
+        BIND_SYMBOL(dl_handle, hmac_update_fptr, "HMAC_Update", 
+            (int(*)(void *, const unsigned char *, size_t)) );
+        BIND_SYMBOL(dl_handle, hmac_final_fptr, "HMAC_Final", 
+            (int(*)(void *, unsigned char *, unsigned int *)) );
+        BIND_SYMBOL(dl_handle, hmac_ctx_free_fptr, "HMAC_CTX_free", 
+            (void(*)(void *)) );
       } else {
-        BIND_SYMBOL(dl_handle, evp_mac_fetch_fptr, "EVP_MAC_fetch", (void*(*)(void *, const char *, const char *)) );
-        BIND_SYMBOL(dl_handle, evp_mac_ctx_new_fptr, "EVP_MAC_CTX_new", (void*(*)(void *)) );
-        BIND_SYMBOL(dl_handle, ossl_param_construct_utf8_string_fptr, "OSSL_PARAM_construct_utf8_string", 
-        (OSSL_PARAM_OSSL3_SHIM(*)(const char *, char *, size_t)) );
-        BIND_SYMBOL(dl_handle, ossl_param_construct_end_fptr, "OSSL_PARAM_construct_end", (OSSL_PARAM_OSSL3_SHIM(*)(void)) );
-        BIND_SYMBOL(dl_handle, evp_mac_init_fptr, "EVP_MAC_init", (int(*)(void *, const unsigned char *, size_t, const OSSL_PARAM_OSSL3_SHIM [])) );
-        BIND_SYMBOL(dl_handle, evp_mac_update_fptr, "EVP_MAC_update", (int(*)(void *, const unsigned char *, size_t)) );
-        BIND_SYMBOL(dl_handle, evp_mac_final_fptr, "EVP_MAC_final", (int(*)(void *, unsigned char *, size_t *, size_t)) );
+        BIND_SYMBOL(dl_handle, evp_mac_fetch_fptr, "EVP_MAC_fetch", 
+            (void*(*)(void *, const char *, const char *)) );
+        BIND_SYMBOL(dl_handle, evp_mac_ctx_new_fptr, "EVP_MAC_CTX_new", 
+            (void*(*)(void *)) );
+        BIND_SYMBOL(dl_handle, ossl_param_construct_utf8_string_fptr, 
+            "OSSL_PARAM_construct_utf8_string", 
+            (OSSL_PARAM_OSSL3_SHIM(*)(const char *, char *, size_t)) );
+        BIND_SYMBOL(dl_handle, ossl_param_construct_end_fptr, 
+            "OSSL_PARAM_construct_end", (OSSL_PARAM_OSSL3_SHIM(*)(void)) );
+        BIND_SYMBOL(dl_handle, evp_mac_init_fptr, "EVP_MAC_init", 
+            (int(*)(void *, const unsigned char *, size_t, 
+            const OSSL_PARAM_OSSL3_SHIM [])) );
+        BIND_SYMBOL(dl_handle, evp_mac_update_fptr, "EVP_MAC_update", 
+            (int(*)(void *, const unsigned char *, size_t)) );
+        BIND_SYMBOL(dl_handle, evp_mac_final_fptr, "EVP_MAC_final", 
+            (int(*)(void *, unsigned char *, size_t *, size_t)) );
       }
     }
   }
@@ -109,7 +129,8 @@ namespace azure {  namespace storage_lite {
     return (*hmac_ctx_reset_fptr)(ctx);
   }
 
-  int HMAC_Init_ex_ossl1_shim(void *ctx, const void *key, int key_len, const void *md, void *impl) {
+  int HMAC_Init_ex_ossl1_shim(void *ctx, const void *key, int key_len, 
+      const void *md, void *impl) {
     return ((*hmac_init_ex_fptr)(ctx, key, key_len, md, impl));
   }
 
@@ -125,7 +146,8 @@ namespace azure {  namespace storage_lite {
     return ((*hmac_ctx_free_fptr)(ctx));
   }
 
-  void* EVP_MAC_fetch_ossl3_shim(void *libctx, const char *algorithm, const char *properties) {
+  void* EVP_MAC_fetch_ossl3_shim(void *libctx, const char *algorithm, 
+      const char *properties) {
     return ((*evp_mac_fetch_fptr)(libctx, algorithm, properties));
   }
 
@@ -133,7 +155,8 @@ namespace azure {  namespace storage_lite {
     return ((*evp_mac_ctx_new_fptr)(mac));
   }
 
-  OSSL_PARAM_OSSL3_SHIM OSSL_PARAM_construct_utf8_string_ossl3_shim(const char *key, char *buf, size_t bsize) {
+  OSSL_PARAM_OSSL3_SHIM OSSL_PARAM_construct_utf8_string_ossl3_shim
+      (const char *key, char *buf, size_t bsize) {
     return ((*ossl_param_construct_utf8_string_fptr)(key, buf, bsize));
   }
 
@@ -141,7 +164,8 @@ namespace azure {  namespace storage_lite {
     return ((*ossl_param_construct_end_fptr)());
   }
 
-  int EVP_MAC_init_ossl3_shim(void *ctx, const unsigned char *key, size_t keylen, const OSSL_PARAM_OSSL3_SHIM params[]) {
+  int EVP_MAC_init_ossl3_shim(void *ctx, const unsigned char *key, 
+      size_t keylen, const OSSL_PARAM_OSSL3_SHIM params[]) {
     return ((*evp_mac_init_fptr)(ctx, key, keylen, params));
   }
 
